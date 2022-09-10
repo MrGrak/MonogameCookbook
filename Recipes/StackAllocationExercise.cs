f820ec52ddb65420fff082a10b5a97976643a36e
MrGrak <someloudnoise@gmail.com>
2022-09-10 14:56:12 -0500

    stack allocation exercise added

---
+++ Recipes/StackAllocationExercise.cs
@@ -0,0 +1,27 @@
+﻿//C# stack allocation exercise
+
+//is A stack allocated? no, A exists outside of method stack frame 
+int A = 0;
+public void MethodA()
+{
+	A++;
+}
+
+//is B stack allocated? yes, it should be, B is local to method stack frame
+public void MethodB()
+{
+	int B = 0;
+	B++;
+}
+
+//is C stack allocated? probably (?), C is local to first method's stack frame
+public void MethodC()
+{
+	int C = 0;
+	Increment(ref C);
+}
+
+public void Increment(ref int C)
+{
+	C++;
+}
