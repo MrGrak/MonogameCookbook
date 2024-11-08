ac0fdca87249d2ed5a8728d2d9282b711bec3c71
MrGrak <someloudnoise@gmail.com>
2024-11-08 09:18:13 -0600

    public private local example added

---
+++ Private Variables/PublicPrivateLocal.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+
+
+public class @Class
+{
+	//public private local routines
+	public int AddOne(ref int i) { return i++; }
+
+	private int _AddOne(ref int i) { return i++; }
+
+	public int Work()
+	{
+		int i = 0;
+
+		AddOne(ref i);
+		_AddOne(ref i);
+		goto AddOne;
+		@return:
+		
+		return i;
+		
+		AddOne:
+		{ i++; goto @return; }
+	}
+}
+
+
+
+
+
+
